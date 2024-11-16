407ab2379ada2fe4b5bc313d6f4a7f26de3f7638
Purushottam <pkccse@outlook.com>
2024-11-16 13:26:17 +0530

    DictionaryAndTuple
    TestDictionaryAndTuple

---
+++ CSharp/DataStructures/CSharpDataStructures/Program.cs
@@ -0,0 +1,20 @@
+﻿using CSharpDataStructures;
+
+internal class Program
+{
+    /// <summary>
+    /// This the main, using it for nessesary calls only
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        #region Calling DictionaryAndTuple
+        var dictionaryAndTuple = new DictionaryAndTuple();
+        dictionaryAndTuple.TestDictionaryAndTuple();
+        #endregion
+
+        #region Future calls
+
+        #endregion
+    }
+}
